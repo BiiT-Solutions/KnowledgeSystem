69c0beab8dba8f6135abcc278c6f2074d6bc6e5b
Jorge Hortelano <jhortelano@biit-solutions.com>
2023-12-05 17:12:21 +0100

    mach any

--- knowledge-system-core/src/test/java/com/biit/ks/core/opensearch/OpenSearchMixedSearchTests.java
+++ knowledge-system-core/src/test/java/com/biit/ks/core/opensearch/OpenSearchMixedSearchTests.java
@@ -1,12 +1,15 @@
 package com.biit.ks.core.opensearch;
 
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
-import org.opensearch.client.opensearch._types.query_dsl.Intervals;
-import org.opensearch.client.opensearch._types.query_dsl.IntervalsAllOf;
+import org.opensearch.client.opensearch._types.query_dsl.BoolQuery;
+import org.opensearch.client.opensearch._types.query_dsl.IntervalsAnyOf;
 import org.opensearch.client.opensearch._types.query_dsl.IntervalsMatch;
 import org.opensearch.client.opensearch._types.query_dsl.IntervalsPrefix;
 import org.opensearch.client.opensearch._types.query_dsl.IntervalsQuery;
 import org.opensearch.client.opensearch._types.query_dsl.IntervalsQueryBuilders;
+import org.opensearch.client.opensearch._types.query_dsl.IntervalsWildcard;
+import org.opensearch.client.opensearch._types.query_dsl.IntervalsAllOf;
+import org.opensearch.client.opensearch._types.query_dsl.Query;
 import org.opensearch.client.opensearch.core.SearchResponse;
 import org.opensearch.client.opensearch.indices.PutIndicesSettingsResponse;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -17,6 +20,8 @@ import org.testng.annotations.AfterClass;
 import org.testng.annotations.Test;
 
 import java.awt.Color;
+import java.util.ArrayList;
+import java.util.List;
 
 @SpringBootTest
 @Test(groups = {"opensearchClient"})
@@ -48,7 +53,7 @@ public class OpenSearchMixedSearchTests extends AbstractTestNGSpringContextTests
     private static final String DATA4_COLOR = Color.WHITE.toString();
 
     private static final String DATA5_ID = "5";
-    private static final String DATA5_NAME = "fifthData";
+    private static final String DATA5_NAME = "fifthDatum";
     private static final String DATA5_DESCRIPTION = "The final and last Data";
 
     private static final String DATA5_COLOR = Color.BLACK.toString();
@@ -75,24 +80,55 @@ public class OpenSearchMixedSearchTests extends AbstractTestNGSpringContextTests
     }
 
     @Test(dependsOnMethods = "indexData")
-    public void searchDataWithIntervalsAndGap() {
-        //Max gaps 0 --> terms must be next to each other.
-        Intervals intervals = new Intervals.Builder().match(new IntervalsMatch.Builder().useField("description").query("The Data").maxGaps(3).ordered(true)
-                .build()).build();
-
-        IntervalsAllOf allQueries = IntervalsQueryBuilders.allOf().intervals(intervals).build();
-        IntervalsQuery query = new IntervalsQuery.Builder().field("description").allOf(allQueries).build();
-
-        final SearchResponse<Data> response = openSearchClient.searchData(Data.class, query._toQuery());
-        Assert.assertEquals(response.hits().hits().size(), 4);
-    }
-
-    @Test(dependsOnMethods = "indexData")
     public void searchDataWithPrefixOnWords() {
         IntervalsQuery query = new IntervalsQuery.Builder().field("description").prefix(new IntervalsPrefix.Builder().useField("description").prefix("Th").build()).build();
 
         final SearchResponse<Data> response = openSearchClient.searchData(Data.class, query._toQuery());
-        Assert.assertEquals(response.hits().hits().size(), 4);
+        Assert.assertEquals(response.hits().hits().size(), 5);
+    }
+
+    @Test(dependsOnMethods = "indexData")
+    public void searchDataWithIntervalsAndWildCard() {
+        //DATA4 not matching.
+        IntervalsMatch intervalsDescription = new IntervalsMatch.Builder().useField("description").query("The Data").maxGaps(3).ordered(true)
+                .build();
+        //DATA5 not matching.
+        IntervalsWildcard intervalsName = new IntervalsWildcard.Builder().useField("name").pattern("*Data").build();
+
+        IntervalsQuery intervalsDescriptionQuery = new IntervalsQuery.Builder().field("description").match(intervalsDescription).build();
+        IntervalsQuery intervalsNameQuery = new IntervalsQuery.Builder().field("name").wildcard(intervalsName).build();
+
+        List<Query> matchQueries = new ArrayList<>();
+        matchQueries.add(intervalsDescriptionQuery._toQuery());
+        matchQueries.add(intervalsNameQuery._toQuery());
+
+        BoolQuery boolQuery = new BoolQuery.Builder().must(matchQueries).build();
+
+        final SearchResponse<Data> response = openSearchClient.searchData(Data.class, boolQuery._toQuery());
+        Assert.assertEquals(response.hits().hits().size(), 3);
+    }
+
+    @Test(dependsOnMethods = "indexData")
+    public void searchDataWithIntervalsAndWildCardAny() {
+        //DATA4 not matching.
+        IntervalsMatch intervalsDescription = new IntervalsMatch.Builder().useField("description").query("The Data").maxGaps(3).ordered(true)
+                .build();
+        //DATA5 not matching.
+        IntervalsWildcard intervalsName = new IntervalsWildcard.Builder().useField("name").pattern("*Data").build();
+
+        IntervalsQuery intervalsDescriptionQuery = new IntervalsQuery.Builder().field("description").match(intervalsDescription).build();
+        IntervalsQuery intervalsNameQuery = new IntervalsQuery.Builder().field("name").wildcard(intervalsName).build();
+
+
+        IntervalsAnyOf intervalsAnyOf = IntervalsQueryBuilders.anyOf()
+                .intervals(intervalsDescription._toIntervals(), intervalsName._toIntervals()).build();
+
+
+        //Why I need a field here, and can be name or description?
+        IntervalsQuery query = new IntervalsQuery.Builder().field("name").anyOf(intervalsAnyOf).build();
+
+        final SearchResponse<Data> response = openSearchClient.searchData(Data.class, query._toQuery());
+        Assert.assertEquals(response.hits().hits().size(), 5);
     }
 
 
