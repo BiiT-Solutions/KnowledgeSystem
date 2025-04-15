9e030345423564f78ae6773f6641160a1bf67016
Jorge Hortelano <jhortelano@biit-solutions.com>
2025-04-15 13:41:47 +0200

    Reviewed exceptions

--- knowledge-system-rest/src/main/java/com/biit/ks/rest/exceptions/KnowledgeSystemExceptionControllerAdvice.java
+++ knowledge-system-rest/src/main/java/com/biit/ks/rest/exceptions/KnowledgeSystemExceptionControllerAdvice.java
@@ -1,17 +1,22 @@
 package com.biit.ks.rest.exceptions;
 
+import com.biit.kafka.exceptions.InvalidEventException;
 import com.biit.ks.core.exceptions.CategoryAlreadyExistsException;
+import com.biit.ks.core.exceptions.CategoryNotFoundException;
 import com.biit.ks.core.exceptions.FileAlreadyExistsException;
 import com.biit.ks.core.exceptions.FileHandlingException;
 import com.biit.ks.core.exceptions.FileNotFoundException;
 import com.biit.ks.core.exceptions.SeaweedClientException;
 import com.biit.ks.core.exceptions.TextAlreadyExistsException;
+import com.biit.ks.logger.KnowledgeSystemLogger;
 import com.biit.ks.persistence.opensearch.exceptions.OpenSearchException;
 import com.biit.ks.persistence.opensearch.exceptions.OpenSearchInvalidSearchQueryException;
 import com.biit.server.exceptions.ErrorResponse;
 import com.biit.server.exceptions.NotFoundException;
 import com.biit.server.exceptions.ServerExceptionControllerAdvice;
-import com.biit.server.logger.RestServerExceptionLogger;
+import com.biit.usermanager.client.exceptions.ElementNotFoundException;
+import com.biit.usermanager.client.exceptions.InvalidConfigurationException;
+import com.biit.usermanager.client.exceptions.InvalidValueException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
@@ -23,58 +28,87 @@ public class KnowledgeSystemExceptionControllerAdvice extends ServerExceptionCon
 
     @ExceptionHandler(NotFoundException.class)
     public ResponseEntity<Object> notFoundException(Exception ex) {
-        RestServerExceptionLogger.errorMessage(this.getClass().getName(), ex);
-        return new ResponseEntity<>(new ErrorResponse("NOT_FOUND", "not_found", ex), HttpStatus.NOT_FOUND);
+        KnowledgeSystemLogger.errorMessage(this.getClass().getName(), ex);
+        return new ResponseEntity<>(new ErrorResponse(ex.getMessage(), "not_found", ex), HttpStatus.NOT_FOUND);
     }
 
 
     @ExceptionHandler(SeaweedClientException.class)
     public ResponseEntity<Object> seaweedClientException(Exception ex) {
-        RestServerExceptionLogger.errorMessage(this.getClass().getName(), ex);
-        return new ResponseEntity<>(new ErrorResponse("NOT_FOUND", "error_storing_file", ex), HttpStatus.BAD_REQUEST);
+        KnowledgeSystemLogger.errorMessage(this.getClass().getName(), ex);
+        return new ResponseEntity<>(new ErrorResponse(ex.getMessage(), "error_storing_file", ex), HttpStatus.BAD_REQUEST);
     }
 
     @ExceptionHandler(OpenSearchException.class)
     public ResponseEntity<Object> openSearchException(Exception ex) {
-        RestServerExceptionLogger.errorMessage(this.getClass().getName(), ex);
-        return new ResponseEntity<>(new ErrorResponse("INTERNAL_SERVER_ERROR", "open_search_exception", ex), HttpStatus.INTERNAL_SERVER_ERROR);
+        KnowledgeSystemLogger.errorMessage(this.getClass().getName(), ex);
+        return new ResponseEntity<>(new ErrorResponse(ex.getMessage(), "open_search_exception", ex), HttpStatus.INTERNAL_SERVER_ERROR);
     }
 
     @ExceptionHandler(FileHandlingException.class)
     public ResponseEntity<Object> fileHandlingException(Exception ex) {
-        RestServerExceptionLogger.errorMessage(this.getClass().getName(), ex);
-        return new ResponseEntity<>(new ErrorResponse("INTERNAL_SERVER_ERROR", "error_handling_files", ex), HttpStatus.INTERNAL_SERVER_ERROR);
+        KnowledgeSystemLogger.errorMessage(this.getClass().getName(), ex);
+        return new ResponseEntity<>(new ErrorResponse(ex.getMessage(), "error_handling_files", ex), HttpStatus.INTERNAL_SERVER_ERROR);
     }
 
     @ExceptionHandler(FileNotFoundException.class)
     public ResponseEntity<Object> fileNotFoundException(Exception ex) {
-        RestServerExceptionLogger.errorMessage(this.getClass().getName(), ex);
-        return new ResponseEntity<>(new ErrorResponse("NOT_FOUND", "file_not_found", ex), HttpStatus.NOT_FOUND);
+        KnowledgeSystemLogger.errorMessage(this.getClass().getName(), ex);
+        return new ResponseEntity<>(new ErrorResponse(ex.getMessage(), "file_not_found", ex), HttpStatus.NOT_FOUND);
     }
 
     @ExceptionHandler(OpenSearchInvalidSearchQueryException.class)
     public ResponseEntity<Object> openSearchInvalidSearchQueryException(Exception ex) {
-        RestServerExceptionLogger.errorMessage(this.getClass().getName(), ex);
-        return new ResponseEntity<>(new ErrorResponse("INTERNAL_SERVER_ERROR", "invalid_query", ex), HttpStatus.INTERNAL_SERVER_ERROR);
+        KnowledgeSystemLogger.errorMessage(this.getClass().getName(), ex);
+        return new ResponseEntity<>(new ErrorResponse(ex.getMessage(), "invalid_query", ex), HttpStatus.INTERNAL_SERVER_ERROR);
     }
 
     @ExceptionHandler(FileAlreadyExistsException.class)
     public ResponseEntity<Object> fileAlreadyExistsException(Exception ex) {
-        RestServerExceptionLogger.errorMessage(this.getClass().getName(), ex);
-        return new ResponseEntity<>(new ErrorResponse("BAD_REQUEST", "file_already_exists", ex), HttpStatus.BAD_REQUEST);
+        KnowledgeSystemLogger.errorMessage(this.getClass().getName(), ex);
+        return new ResponseEntity<>(new ErrorResponse(ex.getMessage(), "file_already_exists", ex), HttpStatus.BAD_REQUEST);
     }
 
     @ExceptionHandler(CategoryAlreadyExistsException.class)
     public ResponseEntity<Object> categoryAlreadyExistsException(Exception ex) {
-        RestServerExceptionLogger.errorMessage(this.getClass().getName(), ex);
-        return new ResponseEntity<>(new ErrorResponse("BAD_REQUEST", "category_already_exists", ex), HttpStatus.BAD_REQUEST);
+        KnowledgeSystemLogger.errorMessage(this.getClass().getName(), ex);
+        return new ResponseEntity<>(new ErrorResponse(ex.getMessage(), "category_already_exists", ex), HttpStatus.BAD_REQUEST);
+    }
+
+    @ExceptionHandler(CategoryNotFoundException.class)
+    public ResponseEntity<Object> categoryNotFoundException(Exception ex) {
+        KnowledgeSystemLogger.errorMessage(this.getClass().getName(), ex);
+        return new ResponseEntity<>(new ErrorResponse(ex.getMessage(), "category_not_found", ex), HttpStatus.NOT_FOUND);
     }
 
     @ExceptionHandler(TextAlreadyExistsException.class)
     public ResponseEntity<Object> textAlreadyExistsException(Exception ex) {
-        RestServerExceptionLogger.errorMessage(this.getClass().getName(), ex);
-        return new ResponseEntity<>(new ErrorResponse("BAD_REQUEST", "text_already_exists_with_name", ex), HttpStatus.BAD_REQUEST);
+        KnowledgeSystemLogger.errorMessage(this.getClass().getName(), ex);
+        return new ResponseEntity<>(new ErrorResponse(ex.getMessage(), "text_already_exists_with_this_name", ex), HttpStatus.BAD_REQUEST);
     }
 
+    @ExceptionHandler(InvalidEventException.class)
+    public ResponseEntity<Object> invalidEventException(Exception ex) {
+        KnowledgeSystemLogger.errorMessage(this.getClass().getName(), ex);
+        return new ResponseEntity<>(new ErrorResponse(ex.getMessage(), "cannot_connect_to_kafka", ex), HttpStatus.INTERNAL_SERVER_ERROR);
+    }
+
+    @ExceptionHandler(ElementNotFoundException.class)
+    public ResponseEntity<Object> elementNotFoundException(Exception ex) {
+        KnowledgeSystemLogger.errorMessage(this.getClass().getName(), ex);
+        return new ResponseEntity<>(new ErrorResponse(ex.getMessage(), "not_found", ex), HttpStatus.NOT_FOUND);
+    }
+
+    @ExceptionHandler(InvalidConfigurationException.class)
+    public ResponseEntity<Object> invalidConfigurationException(Exception ex) {
+        KnowledgeSystemLogger.errorMessage(this.getClass().getName(), ex);
+        return new ResponseEntity<>(new ErrorResponse(ex.getMessage(), "invalid_configuration_exception", ex), HttpStatus.INTERNAL_SERVER_ERROR);
+    }
+
+    @ExceptionHandler(InvalidValueException.class)
+    public ResponseEntity<Object> invalidValueException(Exception ex) {
+        KnowledgeSystemLogger.errorMessage(this.getClass().getName(), ex);
+        return new ResponseEntity<>(new ErrorResponse(ex.getMessage(), "invalid_parameter", ex), HttpStatus.BAD_REQUEST);
+    }
 
 }
