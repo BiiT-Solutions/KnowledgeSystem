ca10c662f4265127f6bc7d1b17bc91c30a3b12cf
Jorge Hortelano <jhortelano@biit-solutions.com>
2025-10-30 16:04:10 +0100

    Removed class

--- knowledge-system-rest-client/src/test/java/com/biit/ks/TestKnowledgeSystemServer.java
+++ knowledge-system-rest-client/src/test/java/com/biit/ks/TestKnowledgeSystemServer.java
@@ -33,7 +33,7 @@ import org.springframework.context.annotation.FilterType;
 
 @SpringBootApplication(exclude = {DataSourceAutoConfiguration.class, HibernateJpaAutoConfiguration.class})
 @ComponentScan(basePackages = {"com.biit.ks", "com.biit.server", "com.biit.usermanager.client", "com.biit.messagebird.client", "com.biit.server.client"},
-        excludeFilters = {@ComponentScan.Filter(type = FilterType.ASSIGNABLE_TYPE, value = {UserGuardDatabaseConfigurator.class})})
+        excludeFilters = {@ComponentScan.Filter(type = FilterType.ASSIGNABLE_TYPE)})
 @ConfigurationPropertiesScan({"com.biit.ks"})
 public class TestKnowledgeSystemServer {
 
